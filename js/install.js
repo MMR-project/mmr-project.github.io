13c3740b29b499f292dc6ed34768af297de89132
N.Shimizu <chikoski@gmail.com>
2014-01-21 13:36:50 +0900

    updated its style with Bootstrap

--- js/install.js
+++ js/install.js
@@ -3,26 +3,40 @@ window.addEventListener("load", function(){
 		return navigator.mozApps && navigator.mozApps.install;
 	};
 
+	var ERROR_MESSAGE = {
+		DENIED: "インストールがキャンセルされたため",
+		INVALID_MANIFEST: "必要な項目がマニフェストに記載されていないため",
+		MANIFEST_URL_ERROR: "マニフェストを取得できなかったため",		
+		NETWORK_ERROR: "マニフェストを取得できなかったため",
+		MANIFEST_PARSE_ERROR: "マニフェストを処理できなかったため",
+		UNKNOWN: "不明なエラーのため"
+	};
+
+	var translateError = function(error){
+		return ERROR_MESSAGE[error.name] || ERROR_MESSAGE.UNKNOWN;
+	};
+
 	var createInstallAction = function(manifest, name){
 		return function(event){
 			event.preventDefault();
 			if(manifest !== null){
 				var response = navigator.mozApps.install(manifest);
 				response.onsuccess = function(data){
-					alert("インストールできました:" + (name || manifest));
+					alert((name || manifest) + "をインストールしました。");
 				};
 				response.onerror = function(){
-					alert("インストールできませんできした:" + (name || manifest));
+					var err = this.error;
+					alert(translateError(err) + "、インストールできませんできした");
 				};
 			}
 		};
 	};
 
 	if(isInstallable()){
-		var link = document.getElementsByClassName("install");
+		var link = document.getElementsByClassName("install-app");
 		for(var i = 0; i < link.length; i++){
 			var manifest = link[i].getAttribute("href");
-			var name = link[i].textContent;
+			var name = link[i].getAttribute("data-app-name");
 			link[i].addEventListener("click", createInstallAction(manifest, name));
 		}
 	}
